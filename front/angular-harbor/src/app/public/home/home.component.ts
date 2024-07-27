6126c486fdc2d9d9f925252fd3c8c9cd80e42817
Kaindall <gustavozanete2@gmail.com>
2024-07-27 18:37:33 -0300

    feature: pagina de login, cadastro, home, contato e sobre tudo certo nos roteamentos. Importado assets de imagens, fundos e texturas

--- front/angular-harbor/src/app/public/home/home.component.ts
+++ front/angular-harbor/src/app/public/home/home.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-home',
-    template: '<app-navbar /><hr><h1>Hello world</h1>'
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.css'
 })
 
 export class HomeComponent {}
--- front/angular-harbor/src/app/public/public.module.ts
+++ front/angular-harbor/src/app/public/public.module.ts
@@ -5,9 +5,11 @@ import { PublicRoutingModule } from "./public-routing.module";
 import { HomeComponent } from "./home/home.component";
 import { AboutComponent } from "./about/about.component";
 import { ContactComponent } from "./contact/contact.component";
+import { LoginComponent } from "./login/login.component";
+import { SignupComponent } from "./signup/signup.component";
 
 @NgModule({
-    declarations: [HomeComponent, AboutComponent, ContactComponent],
+    declarations: [HomeComponent, AboutComponent, ContactComponent, LoginComponent, SignupComponent],
     imports: [PublicRoutingModule, SharedModule]
 })
 
