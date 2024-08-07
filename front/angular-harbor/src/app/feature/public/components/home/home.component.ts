99d8dc1713ba64ffe5a633efe2d3d6c3327c416f
Kaindall <gustavozanete2@gmail.com>
2024-08-06 23:18:19 -0300

    FEAT: reestruturação geral da temática do site

--- front/angular-harbor/src/app/feature/public/components/home/home.component.ts
+++ front/angular-harbor/src/app/feature/public/components/home/home.component.ts
@@ -10,38 +10,4 @@ import { BooksService } from "../../services/books.service";
 })
 
 export class HomeComponent {
-    constructor (
-        private booksService: BooksService,
-        private router: Router,
-        private render: Renderer2) {}
-
-    addFile(event: Event) {
-        const inputElement = event.target as HTMLInputElement;
-        const formElement: HTMLElement | null = inputElement!.parentElement;
-        const mainElement: HTMLElement | null = formElement!.parentElement;
-
-        let files: FileList | null = inputElement.files;
-
-        if (!files){
-            console.log('Nenhum arquivo adicionado'); 
-            return
-        }
-
-        for (let element = 0; element < files.length; element++) {
-            if (!this.booksService.exists(files[element]['name'])) {
-                this.booksService.addBook(files[element]);
-            }
-        }
-
-        this.router.navigate(['/upload'])
-
-        /* this.render.addClass(formElement, 'zoomEffect');
-        setTimeout(()=>{
-            this.render.addClass(mainElement, 'zoomEffectSlow')
-        }, 300);
-
-        console.log(inputElement.parentElement?.parentElement?.parentElement);
-        setTimeout(()=>{ this.router.navigate(['/upload']); }, 400) */
-        
-    }; 
 }
--- front/angular-harbor/src/app/feature/public/components/upload/upload.component.ts
+++ front/angular-harbor/src/app/feature/public/components/upload/upload.component.ts
@@ -1,15 +1,46 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, Renderer2 } from "@angular/core";
 import { BooksService } from "../../services/books.service";
+import { Router } from "@angular/router";
 
 @Component({
-    selector: "app-upload-books",
+    selector: "app-upload",
     templateUrl: './upload.component.html',
     styleUrl: './upload.component.css',
 })
 
 export class UploadComponent {
 
-    constructor (public booksService: BooksService) {
-    }
+    constructor (
+        private booksService: BooksService,
+        private router: Router,
+        private render: Renderer2) {}
+
+    addFile(event: Event) {
+        const inputElement = event.target as HTMLInputElement;
+        const formElement: HTMLElement | null = inputElement!.parentElement;
+        const mainElement: HTMLElement | null = formElement!.parentElement;
+
+        let files: FileList | null = inputElement.files;
+
+        if (!files){
+            console.log('Nenhum arquivo adicionado'); 
+            return
+        }
+
+        for (let element = 0; element < files.length; element++) {
+            if (!this.booksService.exists(files[element]['name'])) {
+                this.booksService.addBook(files[element]);
+            }
+        }
+
+        /* this.render.addClass(formElement, 'zoomEffect');
+        setTimeout(()=>{
+            this.render.addClass(mainElement, 'zoomEffectSlow')
+        }, 300);
+
+        console.log(inputElement.parentElement?.parentElement?.parentElement);
+        setTimeout(()=>{ this.router.navigate(['/upload']); }, 400) */
+        
+    }; 
 
 }
